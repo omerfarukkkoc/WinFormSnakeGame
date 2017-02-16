9595991033dde4896084d57e17fb6b8dd5bd7492
Ömer Faruk KOÇ <omerfarukkoc42@gmail.com>
2017-02-16 22:06:04 +0300

    Add files via upload

---
+++ yilanOyunu V2/yilanOyunu/yilanOyunu.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace yilanOyunu
+{
+    partial class yilanOyunu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_BaslatDurdur = new MetroFramework.Controls.MetroButton();
+            this.btn_Sifirla = new MetroFramework.Controls.MetroButton();
+            this.lbl_Skor = new MetroFramework.Controls.MetroLabel();
+            this.rb_Hiz1 = new MetroFramework.Controls.MetroRadioButton();
+            this.rb_Hiz2 = new MetroFramework.Controls.MetroRadioButton();
+            this.rb_Hiz3 = new MetroFramework.Controls.MetroRadioButton();
+            this.rb_Hiz4 = new MetroFramework.Controls.MetroRadioButton();
+            this.rb_Hiz5 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(23, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 356);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_BaslatDurdur
+            // 
+            this.btn_BaslatDurdur.Location = new System.Drawing.Point(23, 443);
+            this.btn_BaslatDurdur.Name = "btn_BaslatDurdur";
+            this.btn_BaslatDurdur.Size = new System.Drawing.Size(75, 23);
+            this.btn_BaslatDurdur.TabIndex = 1;
+            this.btn_BaslatDurdur.Text = "OYNA";
+            this.btn_BaslatDurdur.UseSelectable = true;
+            this.btn_BaslatDurdur.Click += new System.EventHandler(this.btn_BaslatDurdur_Click);
+            // 
+            // btn_Sifirla
+            // 
+            this.btn_Sifirla.Location = new System.Drawing.Point(119, 443);
+            this.btn_Sifirla.Name = "btn_Sifirla";
+            this.btn_Sifirla.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sifirla.TabIndex = 2;
+            this.btn_Sifirla.Text = "SIFIRLA";
+            this.btn_Sifirla.UseSelectable = true;
+            this.btn_Sifirla.Click += new System.EventHandler(this.btn_Sifirla_Click);
+            // 
+            // lbl_Skor
+            // 
+            this.lbl_Skor.AutoSize = true;
+            this.lbl_Skor.Location = new System.Drawing.Point(226, 443);
+            this.lbl_Skor.Name = "lbl_Skor";
+            this.lbl_Skor.Size = new System.Drawing.Size(0, 0);
+            this.lbl_Skor.TabIndex = 3;
+            // 
+            // rb_Hiz1
+            // 
+            this.rb_Hiz1.AutoSize = true;
+            this.rb_Hiz1.Location = new System.Drawing.Point(119, 497);
+            this.rb_Hiz1.Name = "rb_Hiz1";
+            this.rb_Hiz1.Size = new System.Drawing.Size(29, 15);
+            this.rb_Hiz1.TabIndex = 4;
+            this.rb_Hiz1.Text = "1";
+            this.rb_Hiz1.UseSelectable = true;
+            this.rb_Hiz1.CheckedChanged += new System.EventHandler(this.rb_Hiz1_CheckedChanged);
+            // 
+            // rb_Hiz2
+            // 
+            this.rb_Hiz2.AutoSize = true;
+            this.rb_Hiz2.Location = new System.Drawing.Point(163, 497);
+            this.rb_Hiz2.Name = "rb_Hiz2";
+            this.rb_Hiz2.Size = new System.Drawing.Size(29, 15);
+            this.rb_Hiz2.TabIndex = 5;
+            this.rb_Hiz2.Text = "2";
+            this.rb_Hiz2.UseSelectable = true;
+            this.rb_Hiz2.CheckedChanged += new System.EventHandler(this.rb_Hiz2_CheckedChanged);
+            // 
+            // rb_Hiz3
+            // 
+            this.rb_Hiz3.AutoSize = true;
+            this.rb_Hiz3.Location = new System.Drawing.Point(215, 497);
+            this.rb_Hiz3.Name = "rb_Hiz3";
+            this.rb_Hiz3.Size = new System.Drawing.Size(29, 15);
+            this.rb_Hiz3.TabIndex = 6;
+            this.rb_Hiz3.Text = "3";
+            this.rb_Hiz3.UseSelectable = true;
+            this.rb_Hiz3.CheckedChanged += new System.EventHandler(this.rb_Hiz3_CheckedChanged);
+            // 
+            // rb_Hiz4
+            // 
+            this.rb_Hiz4.AutoSize = true;
+            this.rb_Hiz4.Location = new System.Drawing.Point(261, 497);
+            this.rb_Hiz4.Name = "rb_Hiz4";
+            this.rb_Hiz4.Size = new System.Drawing.Size(29, 15);
+            this.rb_Hiz4.TabIndex = 7;
+            this.rb_Hiz4.Text = "4";
+            this.rb_Hiz4.UseSelectable = true;
+            this.rb_Hiz4.CheckedChanged += new System.EventHandler(this.rb_Hiz4_CheckedChanged);
+            // 
+            // rb_Hiz5
+            // 
+            this.rb_Hiz5.AutoSize = true;
+            this.rb_Hiz5.Location = new System.Drawing.Point(306, 497);
+            this.rb_Hiz5.Name = "rb_Hiz5";
+            this.rb_Hiz5.Size = new System.Drawing.Size(29, 15);
+            this.rb_Hiz5.TabIndex = 8;
+            this.rb_Hiz5.Text = "5";
+            this.rb_Hiz5.UseSelectable = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(32, 497);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(81, 19);
+            this.metroLabel1.TabIndex = 9;
+            this.metroLabel1.Text = "Hız Seçiniz : ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // yilanOyunu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 549);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.rb_Hiz5);
+            this.Controls.Add(this.rb_Hiz4);
+            this.Controls.Add(this.rb_Hiz3);
+            this.Controls.Add(this.rb_Hiz2);
+            this.Controls.Add(this.rb_Hiz1);
+            this.Controls.Add(this.lbl_Skor);
+            this.Controls.Add(this.btn_Sifirla);
+            this.Controls.Add(this.btn_BaslatDurdur);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "yilanOyunu";
+            this.Resizable = false;
+            this.Text = "Yılan Oyunu - Ömer Faruk KOÇ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MetroFramework.Controls.MetroButton btn_BaslatDurdur;
+        private MetroFramework.Controls.MetroButton btn_Sifirla;
+        private MetroFramework.Controls.MetroLabel lbl_Skor;
+        private MetroFramework.Controls.MetroRadioButton rb_Hiz1;
+        private MetroFramework.Controls.MetroRadioButton rb_Hiz2;
+        private MetroFramework.Controls.MetroRadioButton rb_Hiz3;
+        private MetroFramework.Controls.MetroRadioButton rb_Hiz4;
+        private MetroFramework.Controls.MetroRadioButton rb_Hiz5;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ yilanOyunu V2/yilanOyunu/yilanOyunu.cs
@@ -0,0 +1,362 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yilanOyunu
+{
+    public partial class yilanOyunu : MetroForm
+    {
+
+        int[,] oyunAlani;
+        int skor=0;
+        int hak=3;
+        bool gameOver = false;
+
+        int direction = 6;
+
+        int xLokasyonu = 21;
+        int yLokasyonu = 15;
+        int xGenisleme = 7;
+        int yGenisleme = 7;
+
+        LinkedList<int> yilanXYon=new LinkedList<int>();
+        LinkedList<int> yilanYYon=new LinkedList<int>();
+
+        Random rastgele=new Random();
+
+        Rectangle kare =new Rectangle();
+        SolidBrush yilanRenk=new SolidBrush(Color.Blue);
+        SolidBrush yemRenk =new SolidBrush(Color.Red);
+        Brush oyunAlaniRenk=new SolidBrush(Color.White);
+
+
+
+
+        public yilanOyunu()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(new TestMessageFilter(this));
+            
+            oyunAlani=new int[50,50];
+
+            for(int i=0 ; i<50 ; i++)
+            {
+                for(int j=0 ; j<50 ; j++)
+                {
+                    oyunAlani[i,j]=0;
+                }
+            }
+
+            lbl_Skor.Text="Skor : 0";
+            rb_Hiz2.Checked=true;
+        }
+
+
+        public void oyunSifirlama()
+        {
+            gameOver=false;
+            timer1.Enabled=false;
+            skor=0;
+            lbl_Skor.Text="SKOR " + skor.ToString();
+            direction=6;
+            xLokasyonu=21;
+            yLokasyonu=15;
+            xGenisleme=7;
+            yGenisleme=7;
+
+            for(int i=0 ; i<50 ; i++)
+            {
+                for(int j=0 ; j<50 ; j++)
+                {
+                    oyunAlani[i,j]=0;
+                }
+            }
+
+            yilanXYon.Clear();
+            yilanYYon.Clear();
+
+            btn_BaslatDurdur.Text="OYNA";
+            panel1.Invalidate();
+        }
+
+        public bool tusKontrol(Keys keyCode)
+        {
+            bool ret=true;
+
+            switch(keyCode)
+            {
+                case Keys.A:
+                    if(direction!=6)
+                    {
+                        direction=4;
+                    }
+                    break;
+                case Keys.D:
+                    if(direction!=4)
+                    {
+                        direction=6;
+                    }
+                    break;
+                case Keys.W:
+                    if(direction!=2)
+                    {
+                        direction=8;
+                    }
+                    break;
+                case Keys.S:
+                    if(direction!=8)
+                    {
+                        direction=2;
+                    }
+                    break;
+
+                default:ret=false;
+                    break;
+            }
+            return ret;
+        }
+
+        private void btn_BaslatDurdur_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled==false && !gameOver)
+            {
+                timer1.Enabled=true;
+                btn_BaslatDurdur.Text="DURDUR";
+            }
+            else
+            {
+                timer1.Enabled=false;
+                btn_BaslatDurdur.Text="BAŞLAT";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(direction==8)
+            {
+                if(yLokasyonu==0)
+                {
+                    yLokasyonu=49;
+                }
+                else
+                {
+                    yLokasyonu--;
+                }
+            }
+
+
+            else if(direction==2)
+            {
+                if(yLokasyonu==49)
+                {
+                    yLokasyonu=0;
+                }
+                else
+                {
+                    yLokasyonu++;
+                }
+            }
+
+
+            else if(direction==4)
+            {
+                if(xLokasyonu==0)
+                {
+                    xLokasyonu=49;
+                }
+                else
+                {
+                    xLokasyonu--;
+                }
+            }
+
+
+            else if(direction==6)
+            {
+                if(xLokasyonu==49)
+                {
+                    xLokasyonu=0;
+                }
+                else
+                {
+                    xLokasyonu++;
+                }
+            }
+
+
+
+            if(oyunAlani[xLokasyonu,yLokasyonu]==1)
+            {
+                timer1.Enabled=false;
+                gameOver=true;
+                lbl_Skor.Text= "SKOR: " + skor.ToString() + " Başarısız!!!";
+            }
+
+
+            if(yilanXYon.Count>10 && oyunAlani[xLokasyonu,yLokasyonu]!=3)
+            {
+                yilanXYon.RemoveLast();
+                yilanYYon.RemoveLast();
+            }
+            else if(oyunAlani[xLokasyonu,yLokasyonu]==3)
+            {
+                xGenisleme=rastgele.Next(50);
+                yGenisleme=rastgele.Next(50);
+
+                while(oyunAlani[xGenisleme,yGenisleme]==1)
+                {
+                    xGenisleme=rastgele.Next(50);
+                    yGenisleme=rastgele.Next(50);
+                }
+
+                skor += 5* hak;
+                lbl_Skor.Text= "SKOR: " + skor.ToString();
+            }
+
+            yilanXYon.AddFirst(xLokasyonu);
+            yilanYYon.AddFirst(yLokasyonu);
+
+            for(int i=0 ; i<50 ; i++)
+            {
+                for(int j=0 ; j<50 ; j++)
+                {
+                    oyunAlani[i,j]=0;
+                }
+            }
+
+            LinkedListNode<int> yilanXYonitem = yilanXYon.First;
+            LinkedListNode<int> yilanYYonitem =yilanYYon.First;
+
+            while(yilanXYonitem!=yilanXYon.Last && yilanYYonitem!=yilanYYon.Last)
+            {
+                if(yilanXYonitem==yilanXYon.First)
+                {
+                    oyunAlani[yilanXYonitem.Value,yilanYYonitem.Value]=2;
+                }
+                else
+                {
+                    oyunAlani[yilanXYonitem.Value,yilanYYonitem.Value]=1;
+                }
+
+                yilanXYonitem=yilanXYonitem.Next;
+                yilanYYonitem=yilanYYonitem.Next;
+            }
+            oyunAlani[xGenisleme,yGenisleme]=3;
+
+            #region panel, Yılan Ve Yem Boyama
+            using (Graphics gr=panel1.CreateGraphics())
+            {
+                using(Image im=new Bitmap(panel1.ClientRectangle.Width, panel1.ClientRectangle.Height))
+                {
+                    using(Graphics g=Graphics.FromImage(im))
+                    {
+                        kare.X=0;
+                        kare.Y=0;
+                        kare.Width=500;
+                        kare.Height=500;
+                        g.FillRectangle(oyunAlaniRenk,kare);
+
+                        kare.Width=10;
+                        kare.Height=10;
+
+
+                        for(int i=0 ; i<50 ; i++)
+                        {
+                            for(int j=0 ; j<50 ; j++)
+                            {
+                                if(oyunAlani[i,j]==1)
+                                {
+                                    kare.X=10*i;
+                                    kare.Y=10*j;
+                                    g.FillRectangle(yilanRenk,kare);
+                                }
+                                else if(oyunAlani[i,j]==2)
+                                {
+                                    kare.X=10*i-2;
+                                    kare.Y=10*j-2;
+                                    kare.Width=14;
+                                    kare.Height=14;
+                                    g.FillRectangle(yilanRenk,kare);
+                                    kare.Width=10;
+                                    kare.Height=10;
+                                }
+                                else if(oyunAlani[i,j]==3)
+                                {
+                                    kare.X=10*i;
+                                    kare.Y=10*j;
+                                    g.FillRectangle(yemRenk,kare);
+                                }
+                            }
+                        }  
+                    }
+                    gr.DrawImage(im,panel1.ClientRectangle);
+                }
+            }
+            #endregion
+        }
+
+        #region radioButton Hız Kontroller
+        private void rb_Hiz1_CheckedChanged(object sender, EventArgs e)
+        {
+            hak=1;
+            timer1.Interval=150;
+        }
+
+        private void rb_Hiz2_CheckedChanged(object sender, EventArgs e)
+        {
+            hak=2;
+            timer1.Interval=100;
+        }
+
+        private void rb_Hiz3_CheckedChanged(object sender, EventArgs e)
+        {
+            hak=4;
+            timer1.Interval=50;
+        }
+
+        private void rb_Hiz4_CheckedChanged(object sender, EventArgs e)
+        {
+            hak=8;
+            timer1.Interval=25;
+        }
+
+        #endregion
+        public class TestMessageFilter: IMessageFilter
+        {
+            private yilanOyunu FOwner;
+
+            const int WM_KEYDOWN=0X100;
+            const int WM_KEYUP=0X101;
+            const int WM_LEFTMOUSEDOWN = 0X201;
+            const int WM_LEFTMOUSEUP = 0X202;
+            const int WM_LEFTMOUSEDBL = 0X203;
+
+            public TestMessageFilter(yilanOyunu aOwner)
+            {
+                FOwner=aOwner;
+            }
+
+            public bool PreFilterMessage(ref Message m)
+            {
+                if(m.Msg==WM_KEYDOWN)
+                    return FOwner.tusKontrol((Keys)(int)m.WParam & Keys.KeyCode);
+                else
+                    return false;
+            }
+        }
+
+        private void btn_Sifirla_Click(object sender, EventArgs e)
+        {
+            oyunSifirlama();
+        }
+
+
+    }
+}
